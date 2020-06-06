1815e923a732504938c67001dd55c7d9beb80736
Jonathan Vazquez <36612168+Zokler@users.noreply.github.com>
2020-06-06 07:32:38 -0500

    asdf
    asdf

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name='Título')
+    description = models.TextField(verbose_name='Descripción')
+    image = models.ImageField(verbose_name='Imagen')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de modificación')
 
 #auto_now_add indica que se agregará la fecha cuando se crea el registro
 #auto_now indica que ese campo se modificará cuando se haga un cambio a un registro existente
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
